93876888f3cf6fa32d2dc44a7c097a9fcf4568b6
Tony Hong <humanhong1001@gmail.com>
2018-05-29 16:00:01 +0200

    chord: rename

--- get_coco_imgs.py
+++ get_coco_imgs.py
@@ -1,10 +1,15 @@
 # get_features.py
-from vgg16_hybrid_places_1365 import VGG16_Hubrid_1365
-from keras.preprocessing import image
-from places_utils import preprocess_input
 import numpy as np
+from keras.preprocessing import image
+from keras.models import Model
 
-model = VGG16_Hubrid_1365(weights='places', include_top=False)
+from vgg16_hybrid_places_1365 import VGG16_Hubrid_1365
+from places_utils import preprocess_input
+
+loc_model = VGG16_Hubrid_1365(weights='places', include_top=True)
+
+
+model = Model(loc_model.input, loc_model.get_layer('drop_fc2').output)
 
 img_path = 'restaurant.jpg'
 img = image.load_img(img_path, target_size=(224, 224))
@@ -14,3 +19,4 @@ x = preprocess_input(x)
 
 features = model.predict(x)
 print features
+print features.shape
